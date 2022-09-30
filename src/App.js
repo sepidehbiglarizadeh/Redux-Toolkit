e5de3a0bfc8166d332e8c3c312e21ee6cc1bfc1b
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-30 19:22:15 +0330

    adding todos component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { store } from "./features/store";
 import "./App.css";
 import { Provider } from "react-redux";
-import CounterComponent from "./components/CounterComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteTodos";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CounterComponent/>
+        <AddTodoForm/>
+        <TodoList/>
+        <TotalCompleteItems/>
       </div>
     </Provider>
   );
